daa3592bca35d224bdf24c879315282ca41cf299
umutdemirel <umutdemirell@gmail.com>
2018-12-30 16:31:26 -0800

    Extract redundancy, before_action method is added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
+
   def index
     @articles = Article.all
   end
@@ -19,11 +21,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     flash[:notice] = "Article was successfully deleted!"
@@ -31,11 +31,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
@@ -45,6 +43,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
